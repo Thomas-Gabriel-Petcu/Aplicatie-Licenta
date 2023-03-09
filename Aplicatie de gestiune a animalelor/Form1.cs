e9d70c02293558fa27b79251935613d2b5b3200a
Thomas-Gabriel-Petcu <73488732+Thomas-Gabriel-Petcu@users.noreply.github.com>
2023-03-09 15:06:47 +0200

    Initialization
    Created windows forms app.

---
+++ Aplicatie de gestiune a animalelor/Form1.cs
@@ -0,0 +1,10 @@
+namespace Aplicatie_de_gestiune_a_animalelor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
